a6ca3354fe987421c8439441775ba2838e0da47e
farukkhan <farukinfo@gmail.com>
2013-11-21 01:16:16 +0600

    Added Class3

---
+++ MCSManagement/BusinessManagement/Class3.cs
@@ -0,0 +1,13 @@
+﻿//Added to work on BIQ-383
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BusinessManagement
+{
+    class Class3
+    {
+    }
+}
